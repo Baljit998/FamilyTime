23dea65e737830bcee55d92635314d678275bb20
Baljit998 <71004677+Baljit998@users.noreply.github.com>
2021-06-01 19:28:52 +0530

    Update register.php

--- register.php
+++ register.php
@@ -1,43 +1,52 @@
 <html>
-<head>
-   <script type='text/javascript' src='https://platform-api.sharethis.com/js/sharethis.js#property=5fec31707004c1001968ef3a&product=sticky-share-buttons' async='async'></script>
-  <script src='https://cdn.scaledrone.com/scaledrone.min.js'></script>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width">
-  <style>
-    body {
-      background: black;
-      display: flex;
-      height: 100vh;
-      margin: 0;
-      align-items: center;
-      justify-content: center;
-      padding: 0 50px;
-      font-family: -apple-system, BlinkMacSystemFont, sans-serif;
-    }
-    video {
-      max-width: calc(50% - 100px);
-      margin: 0 50px;
-      box-sizing: border-box;
-      border-radius: 2px;
-      padding: 0;
-      background: white;
-    }
-    .copy {
-      position: fixed;
-      top: 10px;
-      left: 50%;
-      transform: translateX(-50%);
-      font-size: 16px;
-      color: white;
-    }
-  </style>
-</head>
-<body>
-  <div class="copy">Send your URL to a friend to start a video call</div>
-  <video id="localVideo" autoplay muted></video>
-  <video id="remoteVideo" autoplay></video>
-  
-  <script src="script.js"></script>
-</body>
+    <head>
+        <script type='text/javascript' src='https://platform-api.sharethis.com/js/sharethis.js#property=5fec31707004c1001968ef3a&product=sticky-share-buttons' async='async'></script>
+        <style>
+            .container {
+                position: relative;;
+                width: 1260px;
+                height:520px;
+                border:1px red  solid;
+            }
+            .toolbox {
+                position: absolute;
+                bottom: 0px;
+                border:1px red  solid;
+                width: 100%;
+                height:50px;
+            }
+        </style>
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+        <script src="https://meet.jit.si/external_api.js"></script>
+        <script src="script.js"></script>
+        
+        <script>
+            $(function(){
+                BindEvent();
+
+                $('#btnStart').on('click',function(){
+                    var meetingId = $('#txtMeetingId').val();
+                    var dispNme = $('#txtDispNme').val();
+                    StartMeeting(meetingId,dispNme);
+                });
+            });
+            
+        </script>
+    </head>
+    <body>
+        Display Name: <input id='txtDispNme' type="text"> <br>
+        <button id='btnStart'>Start</button>
+        <input type="text" id="txtMeetingId">
+        <div class="container">
+            <div id="toolbox" class="toolbox" style="display:none;">
+                <button id='btnHangup'>Hangup</button>
+                <button id='btnCustomMic'>Mic</button>
+                <button id='btnCustomCamera'>Camera</button>
+                <button id='btnCustomTileView'>Toggle Tileview</button>
+                <button id='btnScreenShareCustom'>SS</button>
+                <button id='btnCustomchat'>chat</button>
+            </div>
+            <div id='jitsi-meet-conf-container'></div>
+        </div>
+    </body>
 </html>
